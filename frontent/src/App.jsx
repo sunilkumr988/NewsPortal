ab9e496c6a962172b44a23fc88ea0054ed99ae24
Ayushkumar8989 <ayushkumar8709419452@gmail.com>
2025-04-05 09:11:40 +0530

    signup with their functionality

--- frontent/src/App.jsx
+++ frontent/src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster";
 import SignInForm from "./auth/forms/SignInForm";
 import SignUpForm from "./auth/forms/SignUpForm";
 import Home from "./pages/Home";
@@ -22,6 +23,7 @@ const App = () => {
         <Route path="/news" element={<NewsArticles />} />
         <Route path="/post/:postSlug" element={<PostDetails />} />
       </Routes>
+      <Toaster />
     </BrowserRouter>
   );
 };
--- frontent/vite.config.js
+++ frontent/vite.config.js
@@ -1,13 +1,21 @@
-import { fileURLToPath } from "url";
+// import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // __dirname ko ESM me resolve karna
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api/" : {
+        target: "http://localhost:7000" ,
+        secure: false, 
+      }
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
