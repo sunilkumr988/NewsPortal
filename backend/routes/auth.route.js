82042f3a5c3ac62559aaf3513fb4ab067f10be46
Ayushkumar8989 <ayushkumar8709419452@gmail.com>
2025-04-06 07:43:05 +0530

    sign in route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js" // Correct path
-
+import { signup, signin } from "../controllers/auth.controller.js" // Correct path
  // Fixed import path
 
 const router = express.Router();  // Use express.Router()
 
 router.post("/signup", signup);  // Define the signup route
+router.post("/signin", signin);  // Define the signin route
 
 export default router;
