d9b23368aaaa0ff8b34e71e7e99b365290b860ff
Ayushkumar8989 <ayushkumar8709419452@gmail.com>
2025-04-10 17:32:34 +0530

    add post and logout in bottom nav bar

--- frontent/src/components/shared/BottomNavBar.jsx
+++ frontent/src/components/shared/BottomNavBar.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-
-import { useDispatch } from "react-redux";
-
+import React from "react"
 import { signOutSuccess } from "@/redux/user/userSlice"
+import { FaSignOutAlt, FaUserAlt } from "react-icons/fa"
+import { IoIosCreate, IoIosDocument } from "react-icons/io"
+import { useDispatch, useSelector } from "react-redux"
+import { Link } from "react-router-dom"
 
 const BottomNavBar = () => {
 
     const dispatch = useDispatch()
+
+    const { currentUser } = useSelector((state) => state.user)
+
 
       const handleSignout = async () => {
         try {
@@ -28,33 +30,48 @@ const BottomNavBar = () => {
         }
 
       }
-  return (
-    <nav className='md:hidden fixed-0 left-0 right-0 bg-slate-200
-    border-t border-gray-300 p-2 flex justify-around'>
-      <Link to="/" className='flex flex-col items-center text-slate-800'>
-      <FaHome size={20}/>
-        <span className='text-xs'>Home</span>
-      </Link>
+  
 
-      <Link to="/dashboard?tab=profile" className='flex flex-col items-center text-slate-800'>
-      <FaUserAlt size={20}/>
-        <span className='text-xs'>Profile</span>
-      </Link>
+      return (
+        <nav className="md:hidden fixed bottom-0 left-0 right-0 bg-slate-200 border-t border-gray-300 p-2 flex justify-around">
+          <Link
+            to="/dashboard?tab=profile"
+            className="flex flex-col items-center text-slate-800"
+          >
+            <FaUserAlt size={20} />
+            <span className="text-xs">Profile</span>
+          </Link>
+    
+          {currentUser && currentUser.isAdmin && (
+            <Link
+              to="/create-post"
+              className="flex flex-col items-center text-slate-800"
+            >
+              <IoIosCreate size={20} />
+              <span className="text-xs">Create Post</span>
+            </Link>
+          )}
+    
+          {currentUser && currentUser.isAdmin && (
+            <Link
+              to="/dashboard?tab=posts"
+              className="flex flex-col items-center text-slate-800"
+            >
+              <IoIosDocument size={20} />
+              <span className="text-xs">Posts</span>
+            </Link>
+          )}
+    
+          <button
+            className="flex flex-col items-center text-slate-800"
+            onClick={handleSignout}
+          >
+            <FaSignOutAlt size={20} />
+            <span className="text-xs">Logout</span>
+          </button>
+        </nav>
+      )
 
-      <div className="p-4 border-t border-gray-700">
-
-      <button className="flex items-center w-full p-2 hover:bg-slate-300 rounded"
-      onClick={handleSignout}
-      >
-        
-        <FaSignInAlt size={20}/>
-        <span>Logout</span>
-      </button>
-
-      </div>
-
-      </nav>
-  )
 }
 
 export default BottomNavBar
